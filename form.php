b423723303705dbc99e699d8224957bf4d313985
niko898 <wildniko88@gmail.com>
2017-01-31 13:34:15 +0200

    Update form.php

--- form.php
+++ form.php
@@ -1,6 +1,6 @@
 <?php
 if($_POST['submit']) {
-$result = mail("wildniko88@gmail.com","Анкета с сайта","С сайта была получена заявка: \nИмя ".$_POST[name]." \nE-mail ".$_POST[email]);
+$result = mail("egegsegse4@gmail.com","Анкета с сайта","С сайта была получена заявка: \nИмя ".$_POST[name]." \nE-mail ".$_POST[email]);
 
 if ($result){
 echo "<p>Спасибо!<p>";	
